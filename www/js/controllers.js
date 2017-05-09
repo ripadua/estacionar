bba26abbc993ee7838785222f89a2e014af34f63
ricardocrispim <ripadua7@gmail.com>
2017-05-09 07:37:34 -0300

    Ajustes no campo placa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -46,6 +46,10 @@ angular.module('starter.controllers', [])
 .controller('EntradaCtrl', function($scope, $localStorage, $cordovaBarcodeScanner, $ionicPopup, $ionicLoading, $state) {
   
   $scope.container = {};
+  $scope.letra_1 = "";
+  $scope.numero_1 = "";
+  $scope.letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  $scope.numeros = [0,1,2,3,4,5,6,7,8,9];
 
   $scope.$on('$stateChangeSuccess', 
     function(event, toState, toParams, fromState, fromParams){ 
