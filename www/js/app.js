4c03c5391f19e3f369a99b227b6ff891669f1b22
ripadua <ripadua7@gmail.com>
2017-05-22 17:46:47 -0300

    Inclusao da tela de login

--- www/js/app.js
+++ www/js/app.js
@@ -91,10 +91,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
         controller: 'ConfiguracoesCtrl'
       }
     }
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/inicio');
+  $urlRouterProvider.otherwise('/login');
 
 })
 
